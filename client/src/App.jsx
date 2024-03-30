aa966d3bc23a034ddac5157f382c0f781da53ea3
mukundmadhav054 <mukundmadhav054@gmail.com>
2024-03-30 11:32:39 +0530

    created signup page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,25 @@
 import React from "react";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
-import HomePage from "./HomePage";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import HomePage from "./components/HomePage";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
 
 const App = () => {
+  const location = useLocation();
+  const shouldRenderFooter =
+    location.pathname !== "/signup" && location.pathname !== "/login";
+
   return (
     <>
       <Navbar />
-      <HomePage />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      {shouldRenderFooter && <Footer />}
     </>
   );
 };
